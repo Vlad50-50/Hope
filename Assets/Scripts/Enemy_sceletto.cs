f02a6484b8d1cf59d7411753adffde203d4f9a6e
Vlad50-50 <150382920+Vlad50-50@users.noreply.github.com>
2025-06-01 17:49:54 +0300

    Summa:01.06.25
    Scelets

--- Assets/Scripts/Enemy_sceletto.cs
+++ Assets/Scripts/Enemy_sceletto.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Enemy_sceletto : MonoBehaviour
 {
-    
+    public Transform target;   // Целевой игрок
+    public float speed = 5f;   // Скорость движения
+    void Update()
+    {
+        if (target == null) return;
+        // Направление движения к цели
+        Vector3 direction = (target.position - transform.position).normalized;
+        // Перемещение к цели
+        transform.position += direction * speed * Time.deltaTime;
+    }
 }
